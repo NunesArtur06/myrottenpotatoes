840efb7a5a46cd91761567e68db93ca56baa183f
NunesArtur06 <arturnunes.s06@gmail.com>
2026-09-03 21:57:19 -0300

    Feat: implementação de delete e update

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,47 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    id = params[:id] # recupera o ID da rota URI
+    @movie = Movie.find(id) # busca o filme no banco pelo ID
+  end
+
+  def new
+    # Renderiza a view 'new' por padrão
+  end
+
+  def create
+    @movie = Movie.create!(params.require(:movie).permit(:title, :rating, :release_date, :description))
+    flash[:notice] = "#{@movie.title} foi criado com sucesso!"
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    movie_params = params.require(:movie).permit(:title, :rating, :description, :release_date)
+    @movie.update!(movie_params)
+
+    # Define mensagem temporária e redireciona para os detalhes do filme
+    flash[:notice] = "O filme '#{@movie.title}' foi atualizado com sucesso!"
+    redirect_to movie_path(@movie)
+  end
+
+  def DELETE
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "O filme '#{@movie.title}' foi excluído com sucesso."
+    redirect_to movies_path
+  end
+
+end
--- db/migrate/20260903114839_create_movies.rb
+++ db/migrate/20260903114839_create_movies.rb
@@ -1,6 +1,11 @@
-class CreateMovies < ActiveRecord::Migration[8.1]
+class CreateMovies < ActiveRecord::Migration[6.0] # ou versão correspondente do Rails
   def change
-    create_table :movies do |t|
+    create_table 'movies' do |t| #esses sao os atributos que a tabela movies vai aceitar
+      t.string 'title' #titulo
+      t.string 'rating' #classificacao
+      t.text 'description' #descricao
+      t.datetime 'release_date' #data de lancamento
+      # Adiciona timestamps para o Rails rastrear criação e alteração automaticamente:
       t.timestamps
     end
   end
